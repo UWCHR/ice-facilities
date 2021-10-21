82f8bd79b45fcff091ab257ed729160cc9869b2b
Trung-Anh Nguyen <trunganh1410@gmail.com>
2021-10-21 01:24:38 -0700

    Changes to R script to correctly merge

--- join/note/merging_script.R
+++ join/note/merging_script.R
@@ -1,4 +1,4 @@
-library(dplyr)
+library(tidyverse)
 
 #Add datasets: facilities list, dmcp authorized facilities, and new facilities (after 2017)
 facilities <- read.csv("../input/facil-list.csv.gz", sep = "|")
@@ -6,20 +6,22 @@ dmcp <- read.csv("../input/dmcp-detailed.csv.gz", sep = "|")
 new_facilities <- read.csv("../hand/to_research.csv")
 
 #Data cleaning
-facilities_clean <- facilities %>% select(1:12)
+facilities_clean <- facilities %>% select(1:12) %>% filter(detloc != "Redacted")
 new_facilities_clean <- new_facilities %>% select(1:7) %>%
   rename(detloc = detention_facility_code,
          name = detention_facility,
          type = contract) %>%
   filter(rec_count >= 50) %>%
+  select(-rec_count) %>%
   mutate(over_72 = case_when(over_72 == "Y" ~ TRUE,
                              TRUE ~ FALSE))
 dmcp_clean <- dmcp %>% select(detloc, over_under_72) %>%
   mutate(over_72 = case_when(over_under_72 == "Over 72" ~ TRUE,
                              TRUE ~ FALSE)) %>%
-  select(-over_under_72)
+  select(-over_under_72) %>%
+  add_column(dmcp_auth = TRUE)
 
 #Joining
-facilities_clean <- merge(facilities_clean, dmcp_clean, by = "detloc")
-facilities_both <- bind_rows(facilities_clean, new_facilities_clean)
+facilities_clean <- left_join(facilities_clean, dmcp_clean, by = "detloc")
+facilities_both <- bind_rows(facilities_clean, new_facilities_clean) %>% distinct(detloc, .keep_all = TRUE)
 write.csv(facilities_both, "../output/facilities.csv")
